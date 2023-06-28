53d9089aa5f9b2680b1e37ac4e7a7ce18465135c
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-06-28 22:01:55 +0100

    remove localhost temp

--- src/api.js
+++ src/api.js
@@ -50,10 +50,10 @@ export const checkToken = async (accessToken) => {
   export const getEvents = async () => {
     NProgress.start();
   
-    if (window.location.href.startsWith("http://localhost")) {
-      NProgress.done();
-      return mockData;
-    }
+    // if (window.location.href.startsWith("http://localhost")) {
+    //   NProgress.done();
+    //   return mockData;
+    // }
   
   
     const token = await getAccessToken();
